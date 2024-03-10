639e77bd80e0e19dd7ad446fff2783ba9c5ae75f
조성민 <tjdalsquf8@naver.com>
2024-03-10 14:27:22 +0900

    지하실
    audio 추가

--- Assets/Scipts/animationscript/Table.cs
+++ Assets/Scipts/animationscript/Table.cs
@@ -5,17 +5,30 @@ using UnityEngine;
 public class Table : MonoBehaviour
 {
     Animator _animator;
+    [SerializeField]
+    private AudioClip _openSound;
+    private AudioSource audio;
+
     private void Awake()
     {
         _animator = GetComponent<Animator>();
+        audio = GetComponent<AudioSource>();
     }
 
+    private void Start()
+    {
+        audio.clip = _openSound;
+    }
     // Update is called once per frame
 
 
     public void AniSetBool(bool isOpen)
     {
+        audio.Play();
         _animator.SetBool("isOpen", isOpen);
     }
-    public bool AniGetBool() { return _animator.GetBool("isOpen"); }
+    public bool AniGetBool() {
+        audio.Play();
+        return _animator.GetBool("isOpen"); 
+    }
 }
