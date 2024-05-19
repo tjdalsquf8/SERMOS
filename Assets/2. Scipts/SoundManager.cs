5472c924ca3025f0fa99a8d0a791af96677a5ea6
조성민 <tjdalsquf8@naver.com>
2024-05-19 12:35:36 +0900

    게임씬
    현관문 에셋 교체, 프리펩 설정(도끼 소리)

--- Assets/2. Scipts/SoundManager.cs
+++ Assets/2. Scipts/SoundManager.cs
@@ -6,12 +6,13 @@ using UnityEngine;
 public class SoundManager : MonoBehaviour
 {
     public AudioClip fallSound; // 재생할 오디오 클립
+    private int collisioncount = 0;
 
     void OnCollisionEnter(Collision collision)
     {
         // 충돌한 오브젝트가 모니터링 리스트에 있는지 확인
         {
-            if (!collision.collider.CompareTag("Player"))
+            if (!collision.collider.CompareTag("Player") && collisioncount==0)
             {
             // 충돌 지점에서 소리를 재생
             Vector3 collisionPoint = collision.contacts[0].point;
