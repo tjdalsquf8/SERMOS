84185a78f38796d0f448b295782a54f5830c982d
조성민 <tjdalsquf8@naver.com>
2024-05-21 14:08:28 +0900

    fade in fade out
    GameScene clearpoint 닿을 시 fade in -> End씬 넘어감

--- Assets/2. Scipts/ClearPoint.cs
+++ Assets/2. Scipts/ClearPoint.cs
@@ -1,20 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class ClearPoint : MonoBehaviour
 {
+    public Image Panel;
+    float time = 0f;
+    float F_time = 1f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -22,6 +26,22 @@ public class ClearPoint : MonoBehaviour
         {
             GameManager.instance.SetGameClear(true);
         }
-        // fade ÄÚ·çÆ¾
+        StartCoroutine(FadeFlow());
+
+        IEnumerator FadeFlow()
+        {
+            Panel.gameObject.SetActive(true);
+            Color alpha = Panel.color;
+
+            while (alpha.a < 1f)
+            {
+                time += Time.deltaTime / F_time;
+                alpha.a = Mathf.Lerp(0, 1, time);
+                Panel.color = alpha;
+                yield return null;
+            }
+            yield return null;
+            SceneManager.LoadScene("EndScene");
+        }
     }
 }
